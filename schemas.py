28c9b7369d43b240a25c4afcefdb445cb59e8ca8
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-09-19 20:13:12 -0300

    esquema de validacao e permissao

--- schemas.py
+++ schemas.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 class LoginSchema(BaseModel):
     """ Define como um usuário deve ser enviado para realizar o login.
     """
-    email:  str = "exemplo@exemplo.com"
-    pssw:   str = "1234!@#$"
+    email:  str
+    pssw:   str
 
 class LoginViewSchema(BaseModel):
     """ Define como um login será retornado.
@@ -16,16 +16,24 @@ class LoginViewSchema(BaseModel):
     message: str
 
 class ValidationSchema(BaseModel):
-    """Define como é o objeto para validação de um usuário.
+    """Define como é o objeto enviado para validação de um usuário.
     """
     token:  str = "XXXXX"
 
+class PermissionViewSchema(BaseModel):
+    """Define como é o objeto retornado com as permissões de um usuário.
+    """
+    user:  str = "exemplo@exemplo.com"
+    vendedor: int
+
 class ValidationViewSchema(BaseModel):
-    email:  str = "exemplo@exemplo.com"
+    """Define como é o objeto retornado para validação de um usuário.
+    """
+    user:  str = "exemplo@exemplo.com"
     token:  str = "XXXXX"
     message: str
 
 class ErrorSchema(BaseModel):
-    """ Define como uma mensagem de eero será representada
+    """ Define como uma mensagem de erro será retornada
     """
     message: str
